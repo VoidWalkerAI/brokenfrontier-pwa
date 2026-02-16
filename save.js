b67edb87130c17a4d4b8c31914d15ccbe374cd25
Voyd Walker <mmiller62174@gmail.com>
2026-02-15 19:08:24 -0500

    Create save.js

---
+++ save.js
@@ -0,0 +1,23 @@
+const saveData = {
+  character: {
+    name: "Eli Brogan",
+    hp: 5,
+    maxHp: 13,
+    wounds: 1,
+    stress: 5,
+    grit: 1,
+    instinct: 2,
+    will: 1
+  },
+
+  worldFlags: {
+    awarenessLevel: 0,
+    entityIntegrity: 3,
+    stationPowerOnline: false,
+    civiliansMissing: 0,
+    entityHostile: false,
+    panicThresholdTriggered: false
+  },
+
+  sessionLog: []
+};
